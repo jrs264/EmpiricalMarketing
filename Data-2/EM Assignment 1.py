c4dfd16eb8399f0e351db5ba5a64dd59c2cf744d
jrs264 <j.r.rutgers@student.vu.nl>
2018-11-10 15:09:53 +0100

    new changes

--- Data-2/EM Assignment 1.py
+++ Data-2/EM Assignment 1.py
@@ -20,14 +20,14 @@ def ScatterPlot(x, y):
     a, b = np.polyfit(x, y, 1)
     plt.plot(x, a*x+b, color='red')  
     plt.scatter(x, y)  
-    #plt.show()
+
 
 def TracePlot(y):
     x_values = np.arange(0, 176, 1)
     plt.plot(x_values, y)
     plt.xlabel('Time')
     plt.ylabel('Sales')
-    #plt.show()
+
     
 def main():
     beer = pd.read_excel('berdata1.xls', delimiter=',', skipinitialspace=True)
@@ -58,33 +58,37 @@ def main():
     promo24_beer = beer.loc[:,"PROMO24"]
     
     ScatterPlot(promo21_beer, vol1_beer)
-    ScatterPlot(promo22_beer, vol2_beer)
-    ScatterPlot(promo23_beer, vol3_beer)
-    ScatterPlot(promo24_beer, vol4_beer)
+    # ScatterPlot(promo22_beer, vol2_beer)
+    # ScatterPlot(promo23_beer, vol3_beer)
+    # ScatterPlot(promo24_beer, vol4_beer)
     
     promo21_beer = stats.mstats.winsorize(promo21_beer, limits = 0.025)
     promo22_beer = stats.mstats.winsorize(promo22_beer, limits = 0.025)
     promo23_beer = stats.mstats.winsorize(promo23_beer, limits = 0.025)
     promo24_beer = stats.mstats.winsorize(promo24_beer, limits = 0.025)
+
+    print(promo21_beer)
     
     ScatterPlot(promo21_beer, vol1_beer)
-    ScatterPlot(promo22_beer, vol2_beer)
-    ScatterPlot(promo23_beer, vol3_beer)
-    ScatterPlot(promo24_beer, vol4_beer)
-    
-    TracePlot(vol1_beer)
-    ScatterPlot(price1_beer, vol1_beer)
-    ScatterPlot(price2_beer, vol2_beer)
-    ScatterPlot(price3_beer, vol3_beer)
-    ScatterPlot(price4_beer, vol4_beer)
-    ScatterPlot(promo11_beer, vol1_beer)
-    ScatterPlot(promo12_beer, vol2_beer)
-    ScatterPlot(promo13_beer, vol3_beer)
-    ScatterPlot(promo14_beer, vol4_beer)
-    ScatterPlot(promo21_beer, vol1_beer)
-    ScatterPlot(promo22_beer, vol2_beer)
-    ScatterPlot(promo23_beer, vol3_beer)
-    ScatterPlot(promo24_beer, vol4_beer)
+    # ScatterPlot(promo22_beer, vol2_beer)
+    # ScatterPlot(promo23_beer, vol3_beer)
+    # ScatterPlot(promo24_beer, vol4_beer)
+    #
+    #TracePlot(vol1_beer)
+    # ScatterPlot(price1_beer, vol1_beer)
+    # ScatterPlot(price2_beer, vol2_beer)
+    # ScatterPlot(price3_beer, vol3_beer)
+    # ScatterPlot(price4_beer, vol4_beer)
+    # ScatterPlot(promo11_beer, vol1_beer)
+    # ScatterPlot(promo12_beer, vol2_beer)
+    # ScatterPlot(promo13_beer, vol3_beer)
+    # ScatterPlot(promo14_beer, vol4_beer)
+    # ScatterPlot(promo21_beer, vol1_beer)
+    # ScatterPlot(promo22_beer, vol2_beer)
+    # ScatterPlot(promo23_beer, vol3_beer)
+    # ScatterPlot(promo24_beer, vol4_beer)
+
+    #plt.show()
     
     #sales_frozen = np.vstack((vol1_frozen, vol2_frozen, vol3_frozen, vol4_frozen, promo11_frozen, promo12_frozen, promo13_frozen, promo14_frozen, promo21_frozen, promo22_frozen, promo23_frozen, promo24_frozen)).T
     variables1 = np.vstack((vol1_beer, price1_beer, promo11_beer, promo21_beer)).T
@@ -93,35 +97,35 @@ def main():
     variables4 = np.vstack((vol4_beer, price4_beer, promo14_beer, promo24_beer)).T
     
     #Descriptive statistics
-    print("Statistics daily returns 1:\n", stats.describe(variables1))
-    print("Statistics daily returns 2:\n", stats.describe(variables2))
-    print("Statistics daily returns 3:\n", stats.describe(variables3))
-    print("Statistics daily returns 4:\n", stats.describe(variables4))
-    
-    print(pd.DataFrame(variables1).corr())
-    print(pd.DataFrame(variables2).corr())
-    print(pd.DataFrame(variables3).corr())
-    print(pd.DataFrame(variables4).corr())
+    # print("Statistics daily returns 1:\n", stats.describe(variables1))
+    # print("Statistics daily returns 2:\n", stats.describe(variables2))
+    # print("Statistics daily returns 3:\n", stats.describe(variables3))
+    # print("Statistics daily returns 4:\n", stats.describe(variables4))
+    #
+    # print(pd.DataFrame(variables1).corr())
+    # print(pd.DataFrame(variables2).corr())
+    # print(pd.DataFrame(variables3).corr())
+    # print(pd.DataFrame(variables4).corr())
     
     X1 = np.vstack((price1_beer, promo11_beer, promo21_beer)).T
     X2 = np.vstack((price2_beer, promo12_beer, promo22_beer)).T
     X3 = np.vstack((price3_beer, promo13_beer, promo23_beer)).T
     X4 = np.vstack((price4_beer, promo14_beer, promo24_beer)).T
-    
-    OrdinaryLeastSquares(vol1_beer, X1)
-    OrdinaryLeastSquares(vol2_beer, X2)
-    OrdinaryLeastSquares(vol3_beer, X3)
-    OrdinaryLeastSquares(vol4_beer, X4)
+    #
+    # OrdinaryLeastSquares(vol1_beer, X1)
+    # OrdinaryLeastSquares(vol2_beer, X2)
+    # OrdinaryLeastSquares(vol3_beer, X3)
+    # OrdinaryLeastSquares(vol4_beer, X4)
     
     X1_new = np.vstack((price1_beer, promo21_beer)).T
     X2_new = np.vstack((price2_beer, promo22_beer)).T
     X3_new = np.vstack((price3_beer, promo13_beer, promo23_beer)).T
     X4_new = np.vstack((price4_beer, promo14_beer)).T
     
-    OrdinaryLeastSquares(vol1_beer, X1_new)
-    OrdinaryLeastSquares(vol2_beer, X2_new)
-    OrdinaryLeastSquares(vol3_beer, X3_new)
-    OrdinaryLeastSquares(vol4_beer, X4_new)
+    # OrdinaryLeastSquares(vol1_beer, X1_new)
+    # OrdinaryLeastSquares(vol2_beer, X2_new)
+    # OrdinaryLeastSquares(vol3_beer, X3_new)
+    # OrdinaryLeastSquares(vol4_beer, X4_new)
     
     
     #y = sales_frozen
--- Data-2/EMA1jaimie.py
+++ Data-2/EMA1jaimie.py
@@ -1,19 +1,24 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 import statsmodels.api as sm
 
-def regression(array):
-    res = sm.OLS(endog=beer[array[0]], exog=beer[array[1:len(array)+1]], missing='drop')
+def winsorize(variables, df):
+    for i in range(0,len(variables)):
+        for j in range (0,len(variables[i])):
+            df[variables[i][j]] = stats.mstats.winsorize(df[variables[i][j]], limits=0.025)
+
+def regression(array, df):
+    res = sm.OLS(endog=df[array[0]], exog=df[array[1:len(array)+1]], missing='drop')
     results = res.fit()
     print(results.summary())
 
-
 beer = pd.read_excel('berdata1.xls', delimiter=',', skipinitialspace=True)
-frozen_diner = pd.read_excel('frddata1.xls', delimiter=',', skipinitialspace=True)
 
-beerVariables = [['VOL1', 'PRICE1', 'PROMO11', 'PROMO21'],['VOL2', 'PRICE2', 'PROMO12', 'PROMO22'], ['VOL3', 'PRICE3', 'PROMO13', 'PROMO23'], ['VOL4', 'PRICE4', 'PROMO14', 'PROMO24'] ]
+columnValues = [['VOL1', 'PRICE1', 'PROMO11', 'PROMO21'],['VOL2', 'PRICE2', 'PROMO12', 'PROMO22'], ['VOL3', 'PRICE3', 'PROMO13', 'PROMO23'], ['VOL4', 'PRICE4', 'PROMO14', 'PROMO24'] ]
 
-for i in range(0,len(beerVariables)):
-    regression(beerVariables[i])
+winsorize(columnValues, beer)
+
+for i in range(0,len(columnValues)):
+    regression(columnValues[i], beer)
 
